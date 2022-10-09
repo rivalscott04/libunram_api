ce36119f77e2a09940771dc0e84361d31358bdcc
Your Name <you@example.com>
2022-10-10 00:53:22 +0800

    route ubah

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('guest', 'GuestController@index');
 
 Route::get('/', function () {
-    return view('Welcome');
+    return view('welcome');
 });
 
 // Route::get('guest', function () {
