722514a4db14e86a7bb55ca63b1b9154f6660cb6
Your Name <you@example.com>
2022-10-13 17:34:28 +0800

    test item

--- app/Http/Controllers/api/ItemController.php
+++ app/Http/Controllers/api/ItemController.php
@@ -51,27 +51,27 @@ class ItemController extends Controller
         // return $data;
         if($data){
             // return "ada";
-            if(count($data->biblio->author) < 2){
-                // return "Data Lebih dari satu";
-                return response()->json([
-                    'status' => 'success',
-                    'message' => 'Detail Item Found',
-                    'item_code' => $data->item_code,
-                    'title' => $data->biblio->title,
-                    'publihser_name' => $data->biblio->publisher->publisher_name,
-                    'publish_year' => $data->biblio->publish_year,
-                    'author1' => $data->biblio->author[0]->author_detail->author_name,
-                ], 200);
-            }
+            // if(count($data->biblio->author) < 2){
+            //     // return "Data Lebih dari satu";
+            //     return response()->json([
+            //         'status' => 'success',
+            //         'message' => 'Detail Item Found',
+            //         'item_code' => $data->item_code,
+            //         'title' => $data->biblio->title,
+            //         'publihser_name' => $data->biblio->publisher->publisher_name,
+            //         'publish_year' => $data->biblio->publish_year,
+            //         'author1' => $data->biblio->author[0]->author_detail->author_name,
+            //     ], 200);
+            // }
             return response()->json([
                 'status' => 'success',
                 'message' => 'Detail Item Found',
-                'item_code' => $data->item_code,
-                'title' => $data->biblio->title,
-                'publihser_name' => $data->biblio->publisher->publisher_name,
-                'publish_year' => $data->biblio->publish_year,
-                'author1' => $data->biblio->author[0]->author_detail->author_name,
-                'author2' => $data->biblio->author[1]->author_detail->author_name
+                'item_code' => $data->item_code
+                // 'title' => $data->biblio->title,
+                // 'publihser_name' => $data->biblio->publisher->publisher_name,
+                // 'publish_year' => $data->biblio->publish_year,
+                // 'author1' => $data->biblio->author[0]->author_detail->author_name,
+                // 'author2' => $data->biblio->author[1]->author_detail->author_name
             ], 200);
         }else{
             return response()->json([
